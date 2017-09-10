4841b21c6ceb6e41b4a09208ba08d86edcfad2e6
okgint <okta.ginting@gmail.com>
2017-09-11 05:04:57 +0700

    Tambah data

--- 2.insert.sql
+++ 2.insert.sql
@@ -12,12 +12,15 @@ INSERT INTO UserProfile values(1, 'okta', 'Oktavianus', 'Ginting', 'M', '1981-10
 INSERT INTO MyGroup values(2,'Ginting Corporation');
 INSERT INTO UserProfile values(3, 'benny', 'Benny', 'Ginting', 'M', '1978-1-5');       
 
-INSERT INTO UserProfile(4, 'windoa', 'Winda', 'Purnomo', 'F', '1986-1-1');
-INSERT INTO UserProfile(5, 'Joko', 'Joko', 'Purnomo', 'M', '1974-1-1');
+INSERT INTO UserProfile values(4, 'winda', 'Winda', 'Purnomo', 'F', '1986-1-1');
+INSERT INTO UserProfile values(5, 'joko', 'Joko', 'Purnomo', 'M', '1974-1-1');
 INSERT INTO MyGroup values(6, 'Tetangga');
 
 INSERT INTO GroupMember values(1, 2, true, now()); --Okta ada di group Ginting Corporation
-INSERT INTO GroupMember values(3, 2, true, now());
+INSERT INTO GroupMember values(3, 2, false, now());
+
+INSERT INTO GroupMember values(4, 6, true, now());
+INSERT INTO GroupMember values(5, 6, false, now());
        
 INSERT INTO Playlist values(1, 1, 'Dewa19-Angin', now());
 
